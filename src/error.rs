bc22bf31fa7b7f62081367f792b3ec183d706feb
alex179ohm <alex179ohm@gmail.com>
2019-01-23 15:22:44 +0100

    Add some error and clean error handling

--- src/error.rs
+++ src/error.rs
@@ -1,37 +1,76 @@
-use std::io::Error as ioError;
-use std::fmt;
 use std::str;
-use std::error;
+use std::{error, fmt, io};
+
+use futures::sync::{mpsc, oneshot};
+
+use codec;
 
 #[derive(Debug)]
-pub enum NsqError {
-    IOError(ioError),
+pub enum Error {
+    /// A Non-Specific internal error than prevented and operation from completing
+    Internal(String),
+    IOError(io::Error),
+
+    /// An IO error
+    IO(io::Error),
+
+    /// An parsing/serialising error occurred
+    Value(String, Option<codec::NSQValue>),
+
+    /// An critical Unexpected Error
+    Unexpected(String),
+
+    /// End of Stream connection is broken
+    EndOfStream,
 }
 
-impl fmt::Display for NsqError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            NsqError::IOError(ref err) => write!(f, "IO error: {}", err),
-        }
+pub fn internal<T: Into<String>>(msg: T) -> Error {
+    Error::Internal(msg.into())
+}
+
+pub fn value<T: Into<String>>(msg: T, val: codec::NSQValue) -> Error {
+    Error::Value(msg.into(), Some(val))
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IOError(err)
     }
 }
 
-impl error::Error for NsqError {
+impl From<oneshot::Canceled> for Error {
+    fn from(err: oneshot::Canceled) -> Error {
+        Error::Unexpected(format!("Oneshot was cancelled before use: {}", err))
+    }
+}
+
+impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
-            NsqError::IOError(ref err) => err.description(),
+            Error::IOError(ref err) => err.description(),
+            Error::IO(ref err) => err.description(),
+            Error::Value(ref s, _) => s,
+            Error::Unexpected(ref s) => s,
+            Error::Internal(ref s) => s,
+            Error::EndOfStream => "End of Stream",
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
-            NsqError::IOError(ref err) => Some(err),          
+            Error::IOError(ref err) => Some(err),          
+            Error::IO(ref err) => Some(err),
+            Error::Value(_, _) => None,
+            Error::Internal(_) => None,
+            Error::Unexpected(_) => None,
+            Error::EndOfStream => None,
         }
     }
 }
 
-impl From<ioError> for NsqError {
-    fn from(err: ioError) -> NsqError {
-        NsqError::IOError(err)
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use std::error::Error;
+        fmt::Display::fmt(self.description(), f)
     }
 }
