1fcee96ba637b4b49bed9816e9620d8dad906918
alex179ohm <alex179ohm@gmail.com>
2019-01-23 20:36:01 +0100

    Remove tokio-proto Frame

--- src/codec.rs
+++ src/codec.rs
@@ -1,14 +1,15 @@
+//! And implementation of the NSQ protocol,
+//! Source: https://github.com/benashford/redis-async-rs/blob/master/src/resp.rs
+
 use std::io;
 use std::io::Cursor;
 use std::iter::Iterator;
+use super::error::Error;
 
 use bytes::{Buf, BufMut, BytesMut};
 use tokio_io::codec::{Encoder, Decoder};
-use tokio_proto::streaming::pipeline::Frame;
-use tokio_proto::streaming::{Body, Message};
 use std::str;
 
-use protocol::RequestMessage;
 use response::Message as TypeMessage;
 
 // Header: Size(4-Byte) + FrameType(4-Byte)
@@ -21,170 +22,213 @@ const FRAME_TYPE_MESSAGE: i32 = 0x02;
 
 const HEARTBEAT: &'static str = "_heartbeat_";
 
-#[derive(Clone)]
-pub struct ClientTypeMap<T> {
-   pub inner: T,
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum NSQValue {
+
+    /// An Error form nsqd,
+    Error(String),
+
+    /// An Command that not implement sending msgs.
+    Command(String),
+
+    /// Succefull response from nsqd.
+    Response(String),
+
+    /// Message from nsqd.
+    MuxMsg(i64, String, String),
+
+    /// A simple message (pub or dpub).
+    Msg(String, String),
+
+    /// Multiple message (mpub)
+    MMsg(String, Vec<String>),
+
+    /// nsqd heartbeat msg.
+    Heartbeat,
 }
 
-pub type NsqMessage = Message<RequestMessage, Body<RequestMessage, io::Error>>;
-pub type NsqResponseMessage = Message<String, Body<TypeMessage, io::Error>>;
+impl NSQValue {
+    fn into_result(self) -> Result<NSQValue, Error> {
+        match self {
+            NSQValue::Error(err) => Err(Error::Remote(err)),
+            s => Ok(s),
+        }
+    }
+}
+
 
 /// NSQ codec
-pub struct NsqCodec {
-    pub decoding_head: bool
+pub struct NsqCodec;
+
+fn write_n(buf: &mut BytesMut) {
+    buf.put_u8(b'\n');
 }
 
+fn check_and_reserve(buf: &mut BytesMut, size: usize) {
+    let remaining_bytes = buf.remaining_mut();
+    if remaining_bytes < size {
+        buf.reserve(size);
+    }
+}
+
+fn write_header(buf: &mut BytesMut, size: i32) {
+    buf.put_i32_be(size);
+}
+
+fn write_cmd(buf: &mut BytesMut, cmd: String) {
+    let cmd_as_bytes = cmd.as_bytes();
+    let size = cmd_as_bytes.len() + 1;
+    check_and_reserve(buf, size);
+    buf.extend(cmd_as_bytes);
+    write_n(buf);
+}
+
+/// write msg:
+/// 
+/// <command>\n
+/// [ 4 byte size in bytes as BigEndian i64 ][ N-byte binary data ]
+/// 
+/// command could be PUB or DPUB or any command that send a message too.
+fn write_msg(buf: &mut BytesMut, cmd: String, msg: String) {
+    let cmd_as_bytes = cmd.as_bytes();
+    let msg_as_bytes = msg.as_bytes();
+    let msg_len = msg_as_bytes.len();
+    let size = cmd_as_bytes.len() + 1 + 4 + msg_len;
+    check_and_reserve(buf, size);
+    buf.extend(cmd_as_bytes);
+    write_n(buf);
+    buf.put_u32_be(msg_len as u32);
+    buf.extend(msg_as_bytes);
+}
+
+fn write_mmsg(buf: &mut BytesMut, cmd: String, msgs: Vec<String>) {
+    let cmd_as_bytes = cmd.as_bytes();
+    let size = cmd_as_bytes.len() + 1;
+    check_and_reserve(buf, size);
+    buf.extend(cmd_as_bytes);
+    write_n(buf);
+    for msg in msgs {
+        let msg_as_bytes = msg.as_bytes();
+        let msg_len = msg_as_bytes.len();
+        size = size + 4 + msg_len;
+        check_and_reserve(buf, size);
+        buf.put_u32_be(msg_len as u32);
+        buf.extend(msg_as_bytes);
+    }
+}
+
+
 impl Decoder for NsqCodec {
-    type Item = Frame<String, TypeMessage, io::Error>;
+    type Item = NSQValue;
     type Error = io::Error;
 
-    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, io::Error> {
+    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         let length = buf.len();
 
         if length < HEADER_LENGTH {
-            return Ok(None);
+             return Ok(Some(
+                 NSQValue::Error(
+                     "Packet lenght must be equal or greater to HEADER_LENGHT".to_owned()
+                     )
+             ));
         }
 
         let mut cursor = Cursor::new(buf.clone());
-        let size: i32 = cursor.get_i32_be();
+        let size: usize = cursor.get_i32_be() as usize;
 
-        if length < size as usize {
-            return Ok(None);
+        if length < size {
+            return Ok(
+                Some(
+                    NSQValue::Error(
+                        "Invalid data size".to_owned()
+                    )
+                ));
         }
 
         let frame_type: i32 = cursor.get_i32_be();
 
-        if frame_type == FRAME_TYPE_RESPONSE {
-            // remove the serialized frame from the buffer.
-            buf.split_to(HEADER_LENGTH + length);
-            match str::from_utf8(&cursor.bytes()) {
-                Ok(s) => {
-                    let decoded_message = s.to_string();
+        match frame_type {
+            FRAME_TYPE_RESPONSE => {
+                buf.split_to(HEADER_LENGTH + length);
+                match str::from_utf8(&cursor.bytes()) {
+                    Ok(s) => {
+                        let decoded_message = s.to_string();
 
-                    // TODO: Implement a proper way to handle the heartbeat
-                    if decoded_message == HEARTBEAT && !self.decoding_head {
-                        Ok(Some(self.heartbeat_message()))
-                    } else if decoded_message == HEARTBEAT {
-                        // toggle streaming
-                        Ok(Some(self.streaming_flag()))
-                    } else {
-                        Ok(Some(Frame::Message {
-                            message: decoded_message,
-                            body: false,
-                        }))
+                        // is heartbeat
+                        if decoded_message == HEARTBEAT {
+                            Ok(Some(NSQValue::Heartbeat))
+                        } else {
+                            Ok(Some(
+                                NSQValue::Response(decoded_message)
+                            ))
+                        }
                     }
+                    Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid UTF-8")),
                 }
-                Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid UTF-8")),
-            }
-        } else if frame_type == FRAME_TYPE_ERROR {
-            Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid packet received"))
-        } else if frame_type == FRAME_TYPE_MESSAGE {
-            if self.decoding_head {
-                // toggle streaming
-                Ok(Some(self.streaming_flag()))
-            } else {
-                let timestamp = cursor.get_i64_be(); // timestamp
+            },
+            FRAME_TYPE_ERROR => {
+                Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid packet received"))
+            },
+            FRAME_TYPE_MESSAGE => {
+                let ts = cursor.get_i64_be(); // timestamp
                 let _ = cursor.get_u16_be(); // attempts
- 
-                let data = str::from_utf8(&cursor.bytes()).unwrap().to_string();
-                let (id, body) = data.split_at(16);
 
-                let message = TypeMessage{
-                    timestamp: timestamp,
-                    message_id: id.to_string(),
-                    message_body: body.to_string()
-                };
+                let data = str::from_utf8(&cursor.bytes()).unwrap().to_string();
+                let (id, msg) = data.split_at(16);
 
                 // remove the serialized frame from the buffer.
                 buf.split_to(HEADER_LENGTH + length);
 
                 Ok(Some(
-                    Frame::Body {
-                        chunk: Some(message),
-                    }
+                    NSQValue::MuxMsg(ts, id.to_owned(), msg.to_owned())
                 ))              
-            }
-        } else {
-            Ok(None)
+                },
+            _ => {Ok(None)},
         }
     }
 }
 
-pub type CodecOutputFrame = Frame<RequestMessage, RequestMessage, io::Error>;
 impl Encoder for NsqCodec {
-    type Item = CodecOutputFrame;
+    type Item = NSQValue;
     type Error = io::Error;
 
-    fn encode(&mut self, message: Self::Item, buf: &mut BytesMut) -> io::Result<()> {
-        match message {
-            Frame::Message { message, .. } => {
-                if let Some(version) = message.version {
-                    buf.reserve(version.len());
-                    buf.extend(version.as_bytes());
-                }
-
-                if let Some(header) = message.header {
-                    buf.reserve(header.len() + 1);
-                    buf.extend(header.as_bytes());
-                }
-
-                if let Some(body) = message.body {
-                    let mut buf_32 = Vec::with_capacity(body.len());
-                    let body_len = body.len() as u32;
-                    buf_32.put_u32_be(body_len);
-                    buf_32.put(&body[..]);
-                    buf.extend(buf_32);
-                }
-
-                if let Some(body_messages) = message.body_messages {
-                    let total_bytes = body_messages
-                        .iter()
-                        .map(|message| message.len())
-                        .fold(0, |acc, len| acc + len);
-
-                    let mut buf_32 = Vec::with_capacity(total_bytes);
-                    // [4-byte body size]
-                    let body_len = total_bytes as u32;
-                    buf_32.put_u32_be(body_len);
-                    // [4-byte num messages]
-                    let messages_len = body_messages.len() as u32;
-                    buf_32.put_u32_be(messages_len);
-                    // [ 4-byte message #1 size ][ N-byte binary data ] ...
-                    for message in &body_messages {
-                        let message_len = message.len() as u32;
-                        buf_32.put_u32_be(message_len);
-                        buf_32.put(&message[..]);
-                    }
-                    buf.extend(buf_32);
-                }
-                Ok(())
-            }
-            Frame::Error { error, .. } => Err(error),
-            Frame::Body { .. } => panic!("Streaming of Requests is not currently supported"),
-        }
-    }
-}
-
-impl NsqCodec {    
-    fn heartbeat_message(&mut self) -> Frame<String, TypeMessage, io::Error>
-    {
-        let message = TypeMessage{
-            timestamp: 0,
-            message_id: HEARTBEAT.to_string(),
-            message_body: HEARTBEAT.to_string()
+    fn encode(&mut self, msg: Self::Item, buf: &mut BytesMut) -> Result<(), Self::Error> {
+        let ret = match msg {
+            NSQValue::Command(cmd) => {
+                write_cmd(buf, cmd)
+            },
+            NSQValue::Msg(cmd, msg) => {
+                write_msg(buf, cmd, msg)
+            },
+            NSQValue::MMsg(cmd, msgs) => {
+                write_mmsg(buf, cmd, msgs)
+            },
+            _ => {},
         };
-
-        Frame::Body {
-            chunk: Some(message),
-        } 
-    }
-
-    fn streaming_flag(&mut self) -> Frame<String, TypeMessage, io::Error>
-    {
-        self.decoding_head = false;
-        Frame::Message {
-            message: "".into(),
-            body: true,
-        }
+        Ok(ret)
     }
 }
+
+//impl NsqCodec {    
+//    fn heartbeat_message(&mut self) -> Frame<String, TypeMessage, io::Error>
+//    {
+//        let message = TypeMessage{
+//            timestamp: 0,
+//            message_id: HEARTBEAT.to_string(),
+//            message_body: HEARTBEAT.to_string()
+//        };
+//
+//        Frame::Body {
+//            chunk: Some(message),
+//        } 
+//    }
+//
+//    fn streaming_flag(&mut self) -> Frame<String, TypeMessage, io::Error>
+//    {
+//        self.decoding_head = false;
+//        Frame::Message {
+//            message: "".into(),
+//            body: true,
+//        }
+//    }
+//}
