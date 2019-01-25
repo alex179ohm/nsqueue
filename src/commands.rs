5c40870b0da36cc8bcf302385f03e7771d533c71
alex179ohm <alex179ohm@gmail.com>
2019-01-25 16:21:32 +0100

    Add command related functions

--- src/commands.rs
+++ src/commands.rs
@@ -1,15 +1,58 @@
-pub mod commands {
-    pub const VERSION_2: &'static str = "  V2";
-    
-    pub const PUB: &'static str = "PUB";
-    pub const MPUB: &'static str = "MPUB";
-    pub const DPUB: &'static str = "DPUB";
+use codec::NsqValue;
 
-    pub const SUB: &'static str = "SUB";
+const VERSION: &'static str = "  V";
+const PUB: &'static str = "PUB";
+const MPUB: &'static str = "MPUB";
+const DPUB: &'static str = "DPUB";
+const SUB: &'static str = "SUB";
+const TOUCH: &'static str = "TOUCH";
+const RDY: &'static str = "RDY";
+const FIN: &'static str = "FIN";
+const CLS: &'static str = "CLS";
+const NOP: &'static str = "NOP";
+const IDENTIFY: &'static str = "IDENTIFY";
+const REQ: &'static str = "REQ";
 
-    pub const RDY: &'static str = "RDY";
-    pub const FIN: &'static str = "FIN";
+pub fn nop() -> NsqValue {
+    NsqValue::Command(format!("{}", NOP))
+}
 
-    pub const NOP: &'static str = "NOP";
-    pub const IDENTIFY: &'static str = "IDENTIFY";
+pub fn identify(config: String) -> NsqValue {
+    NsqValue::Msg(format!("{}", IDENTIFY), config)
+}
+
+pub fn fin() -> NsqValue {
+    NsqValue::Command(format!("{}", FIN))
+}
+
+pub fn cls() -> NsqValue {
+    NsqValue::Command(format!("{}", CLS))
+}
+
+pub fn rdy(i: usize) -> NsqValue {
+    NsqValue::Command(format!("{} {}", RDY, i))
+}
+
+pub fn version(v: usize) -> NsqValue {
+    NsqValue::Command(format!("{}{}", VERSION, v))
+}
+
+pub fn touch(id: &str) -> NsqValue {
+    NsqValue::Command(format!("{} {}", TOUCH, id))
+}
+
+pub fn req(id: &str, timeout: &str) -> NsqValue {
+    NsqValue::Command(format!("{} {} {}", REQ, id, timeout))
+}
+
+pub fn publish(topic: &str, msg: &str) -> NsqValue {
+    NsqValue::Msg(format!("{} {}", PUB, topic), msg.to_owned())
+}
+
+pub fn mpub(topic: &str, msgs: Vec<String>) -> NsqValue {
+    NsqValue::MMsg(format!("{} {}", MPUB, topic), msgs)
+}
+
+pub fn dpub(topic: &str, defer_time: &str, msg: &str) -> NsqValue {
+    NsqValue::Msg(format!("{} {} {}", DPUB, topic, defer_time), msg.to_owned())
 }
