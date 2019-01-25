5c899d5d14d6a20f61aa7dc97ef81c241f108bc4
alex179ohm <alex179ohm@gmail.com>
2019-01-25 16:34:22 +0100

    Add use serde_derive

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use serde_derive::{Serialize, Deserialize};
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Config {
     // Identifiers sent to nsqd representing this client
